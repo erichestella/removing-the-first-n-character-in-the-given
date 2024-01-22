064654cb80c3f8fc1bae4cd90a3af94e55ad2f37
erichestella <erichestella05@gmail.com>
2024-01-22 22:46:37 +0800

    add some variable

--- remove the 'n' character.py
+++ remove the 'n' character.py
@@ -1,16 +1,16 @@
 #adding some title 
-print('REMOVING THE FIRST n IN THE WORD.\n')
+print('\nREMOVING THE FIRST n IN THE WORD.\n')
 
 #you can type any word here
 word = input('TYPE THE WORD YOU WANT:')
 
 #this function remove the character in the word you type
 def remove(word, number):
-    print('Original Word :', word)
+    print('\nOriginal Word :', word)
     cancel = word [number:]
     print('\nResult:')
     return cancel
 
-#it defines how many should the function remove in the word you type
+#defines how many should the function remove in the word you type
 print(remove(word, 2))
 print(remove(word, 7))
