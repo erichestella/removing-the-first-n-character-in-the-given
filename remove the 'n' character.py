b2565596af14153d1f5a2d4f37853d6b07c366da
erichestella <erichestella05@gmail.com>
2024-01-22 22:42:22 +0800

    defines how many of the function should remove in the word you type

--- remove the 'n' character.py
+++ remove the 'n' character.py
@@ -4,8 +4,13 @@ print('REMOVING THE FIRST n IN THE WORD.\n')
 #you can type any word here
 word = input('TYPE THE WORD YOU WANT:')
 
+#this function remove the character in the word you type
 def remove(word, number):
     print('Original Word :', word)
     cancel = word [number:]
     print('\nResult:')
     return cancel
+
+#it defines how many should the function remove in the word you type
+print(remove(word, 2))
+print(remove(word, 7))
