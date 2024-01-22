d5d3973cfc700d9dc726809356322041dece5b1a
erichestella <erichestella05@gmail.com>
2024-01-22 22:35:11 +0800

    it removes the character to the word you type

--- remove the 'n' character.py
+++ remove the 'n' character.py
@@ -3,3 +3,9 @@ print('REMOVING THE FIRST n IN THE WORD.\n')
 
 #you can type any word here
 word = input('TYPE THE WORD YOU WANT:')
+
+def remove(word, number):
+    print('Original Word :', word)
+    cancel = word [number:]
+    print('\nResult:')
+    return cancel
